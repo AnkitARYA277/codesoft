a0944f639ff1af62b01f52998d92735381806cfa
Ankit kumar <148327304+AnkitARYA277@users.noreply.github.com>
2024-09-24 20:27:28 +0530

    Add files via upload
    Study Site

---
+++ jvs.js
@@ -0,0 +1,4 @@
+document.getElementById('learnMoreBtn').addEventListener('click', function() {
+       alert('Welcome to Student Hub! Here you can find resources to excel in your studies.');
+   });
+   
